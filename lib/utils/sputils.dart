f627a43bb36c2d1f25144bf457490f0e4dd13990
xuhao <2547881370@qq.com>
2021-04-27 16:51:16 +0800

    utils add 沉浸状态栏 function

--- lib/utils/sputils.dart
+++ lib/utils/sputils.dart
@@ -1,3 +1,7 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SPUtils {
@@ -59,5 +63,33 @@ class SPUtils {
   static bool isLogined() {
     String nickName = getNickName();
     return nickName != null && nickName.isNotEmpty;
+  }
+
+  /// 设置沉浸式状态栏
+  /// 状态栏样式 沉浸式状态栏
+  static statusBar() {
+      // 白色沉浸式状态栏颜色  白色文字
+      SystemUiOverlayStyle light = SystemUiOverlayStyle(
+        systemNavigationBarColor: Color(0xFF000000),
+        systemNavigationBarDividerColor: null,
+        /// 注意安卓要想实现沉浸式的状态栏 需要底部设置透明色
+        statusBarColor: Colors.transparent,
+        systemNavigationBarIconBrightness: Brightness.light,
+        statusBarIconBrightness: Brightness.light,
+        statusBarBrightness: Brightness.dark,
+      );
+
+      // 黑色沉浸式状态栏颜色 黑色文字
+      SystemUiOverlayStyle dark = SystemUiOverlayStyle(
+        systemNavigationBarColor: Color(0xFF000000),
+        systemNavigationBarDividerColor: null,
+        /// 注意安卓要想实现沉浸式的状态栏 需要底部设置透明色
+        statusBarColor: Colors.transparent,
+        systemNavigationBarIconBrightness: Brightness.light,
+        statusBarIconBrightness: Brightness.dark,
+        statusBarBrightness: Brightness.light,
+      );
+      // 这个地方你可以去掉三目运算符 直接调用你想要的 效果即可
+      SystemChrome.setSystemUIOverlayStyle(light);
   }
 }
